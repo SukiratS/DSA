2ce50b165555a0f882556a15eba85991b10fe97b
SukiratS <121662425+SukiratS@users.noreply.github.com>
2023-08-02 04:00:27 +0530

    Create KlargestElements.cpp

---
+++ PriorityQueues/KlargestElements.cpp
@@ -0,0 +1,32 @@
+#include<queue>
+#include<vector>
+
+vector<int> kLargest(int arr[], int n, int k){
+    
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    for(int i=0;i<k;i++)
+    {
+        pq.push(arr[i]);
+    }
+
+    for(int i=k;i<n;i++)
+    {
+        if(arr[i]>pq.top())
+        {
+            pq.push(arr[i]);
+            pq.pop();
+        }
+    }
+
+    vector<int> v;
+
+    while(!pq.empty())
+    {
+        v.push_back(pq.top());
+        pq.pop();
+            }
+
+return v;
+
+}
